0b19c0fbd3f430b238576e25cf06f2ed27787ef4
Guilherme Bezerra <40393853+guilhermebezerrads@users.noreply.github.com>
2020-10-18 19:40:22 -0300

    Commit 12
    Commit 12

--- Movie.java
+++ Movie.java
@@ -37,26 +37,10 @@ public class Movie {
   }
 
   public double getCharge(int daysRented){
-    double thisAmount = 0;
-    switch (getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (daysRented > 2)
-              thisAmount += (daysRented - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += daysRented * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (daysRented > 3)
-              thisAmount += (daysRented - 3) * 1.5;
-            break;
-    }
-    return thisAmount;
-   }
+    return _price.getCharge(daysRented);
+  }
 
-    public int getFrequentRenterPoints(int daysRented) {
+  public int getFrequentRenterPoints(int daysRented) {
       // add bonus for a two day new release rental
       if ((getPriceCode() == NEW_RELEASE) && daysRented > 1){
         return 2;
